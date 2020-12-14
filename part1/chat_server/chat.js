b3212db08ab4b02fd29203f77b741c65b42267a3
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2020-12-14 17:55:31 +0100

    [ADD] name property to each client in chat

--- part1/chat_server/chat.js
+++ part1/chat_server/chat.js
@@ -3,15 +3,21 @@ var net = require('net') // this contains all the tcp stuff
 var chatServer = net.createServer(), clientList = []
 
 chatServer.on('connection', function(client) {
-    client.write('Hi!\n');
+    client.name = client.remoteAddress + ':' + client.remotePort
+    client.write('Hi ' + client.name + '\n');
     clientList.push(client)
     client.on('data', function(data) {
-        for (var i = 0; i < clientList.length; i++) {
-            clientList[i].write(data)
-        }
-        console.log(data)
+        broadcast(data, client)
     })
 })
+
+function broadcast(message, client) {
+    for (var i = 0; i < clientList.length; i++) {
+        if (client !== clientList[i]) {
+            clientList[i].write(client.name + " says " + message)
+        }
+    }
+}
 chatServer.listen(9000)
 
 // to test the client : telnet 127.0.0.1 9000
