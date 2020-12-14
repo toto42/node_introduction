05472588b9d501b25e389203fd92b2a3808f631a
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2020-12-14 18:06:46 +0100

    [ADD] check for the write status of socket in chat

--- part1/chat_server/chat.js
+++ part1/chat_server/chat.js
@@ -2,20 +2,32 @@ var net = require('net') // this contains all the tcp stuff
 
 var chatServer = net.createServer(), clientList = []
 
-chatServer.on('connection', function(client) {
+chatServer.on('connection', function (client) {
     client.name = client.remoteAddress + ':' + client.remotePort
     client.write('Hi ' + client.name + '\n');
     clientList.push(client)
-    client.on('data', function(data) {
+    client.on('data', function (data) {
         broadcast(data, client)
+    })
+    client.on('end', function () {
+        clientList.splice(clientList.indexOf(client), 1)
     })
 })
 
 function broadcast(message, client) {
+    var cleanup = []
     for (var i = 0; i < clientList.length; i++) {
         if (client !== clientList[i]) {
-            clientList[i].write(client.name + " says " + message)
+            if (clientList[i].writable) {
+                clientList[i].write(client.name + " says " + message)
+            } else {
+                cleanup.push(clientList[i])
+                clientList[i].destroy()
+            }
         }
+    }
+    for (i = 0; i < cleanup.length; i++) {
+        clientList.splice(clientList.indexOf(cleanup[i]), 1)
     }
 }
 chatServer.listen(9000)
