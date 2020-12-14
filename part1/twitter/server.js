8ed08d96d7f4addc5ddf80b5530c2ac7facd65fc
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2020-12-14 18:35:41 +0100

    [ADD] extremely basic API on Twitter

--- part1/twitter/server.js
+++ part1/twitter/server.js
@@ -1,8 +1,22 @@
 var express = require('express')
 
 var app = express.createServer()
+app.listen(8000)
 
-app.get('/', function(req, res) {
+var tweets = []
+
+app.get('/', function (req, res) {
     res.send('Welcome to Node Twitter')
 })
-app.listen(8000)
+
+app.post('/send', express.bodyParser(), function (req, res) {
+    if (req.body && req.body.tweet) {
+        tweets.push(req.body.tweet)
+        res.send({ status: "ok", message: "Tweet received" })
+    } else {
+        res.send({ status: "ko", message: "No Tweet received" })
+    }
+})
+app.get('/tweets', function (req, res) {
+    res.send(tweets)
+})
