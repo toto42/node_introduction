3e992fee6b1860b95f9a7ab577a94cf659ef7afe
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2020-12-14 17:49:33 +0100

    [ADD] Communicating between clients in chat

--- part1/chat_server/chat.js
+++ part1/chat_server/chat.js
@@ -1,10 +1,14 @@
 var net = require('net') // this contains all the tcp stuff
 
-var chatServer = net.createServer()
+var chatServer = net.createServer(), clientList = []
 
 chatServer.on('connection', function(client) {
     client.write('Hi!\n');
+    clientList.push(client)
     client.on('data', function(data) {
+        for (var i = 0; i < clientList.length; i++) {
+            clientList[i].write(data)
+        }
         console.log(data)
     })
 })
