b6fa0414512b660a961493a85803e85fd59232b7
toto42 <alexandre.ohayon@epitech.eu>
2020-12-18 17:16:40 +0100

    [ADD] twitter rendering page

--- part1/twitter/app.js
+++ part1/twitter/app.js
@@ -6,15 +6,29 @@ app.listen(8000)
 var tweets = []
 
 app.get('/', function (req, res) {
-    res.send('Welcome to Node Twitter')
+    var title = 'Toto',
+        header = 'Welcome to Toto'
+
+    res.render('index', {
+        locals: {
+            'title': title,
+            'header': header,
+            'tweets': tweets,
+            stylesheets: ['/public/style.css']
+        }
+    })
 })
 
 app.post('/send', express.bodyParser(), function (req, res) {
     if (req.body && req.body.tweet) {
         tweets.push(req.body.tweet)
-        res.send({ status: "ok", message: "Tweet received" })
+        if (acceptsHtml(req.headers['accept'])) {
+            res.redirect('/', 302)
+        } else {
+            res.send({ status: "ok", message: "Tweet received" })
+        }
     } else {
-        res.send({ status: "ko", message: "No Tweet received" })
+        res.send({ status: "nok", message: "No tweet received" })
     }
 })
 app.get('/tweets', function (req, res) {
